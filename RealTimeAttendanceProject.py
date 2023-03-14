ba8e6ee0ab637bcb37739b1b33d47a89dddfa9d6
Mirza Jalal Uddin <38423570+MirzaJalal@users.noreply.github.com>
2023-03-14 02:22:20 +0100

    Final Changes

--- RealTimeAttendanceProject.py
+++ RealTimeAttendanceProject.py
@@ -8,7 +8,7 @@ path = 'TrainingDataset'
 imageArray = []
 studentNames = []
 
-# TODO: reading images from file path
+# reading images from file path
 for singleImageFile in os.listdir(path):
     currentImage =cv2.imread(f'{path}/{singleImageFile}')
     imageArray.append(currentImage)
@@ -16,7 +16,7 @@ for singleImageFile in os.listdir(path):
 
 print("The students are: ", studentNames)
 
-# TODO: to find the image encoding
+# to find the image encoding
 def computeImageEncoding(imageArray):
     encodingList = []
     for image in imageArray:
@@ -27,7 +27,7 @@ def computeImageEncoding(imageArray):
     return encodingList
 print('Encoding is Done!!')
 
-# TODO: attendance marking
+# attendance marking
 def markAttendance(studentName):
     with open('attendanceReport.csv', 'r+') as file:
         data_list = file.readlines()
@@ -45,46 +45,46 @@ encodingKnownList = computeImageEncoding(imageArray)
 print("Total Number of Students: ", len(encodingKnownList))
 print('Waiting for Capturing Image!!')
 
-# TODO: initializing the webcam
+# initializing the webcam
 capture = cv2.VideoCapture(0)
 
-# TODO: loop to iterate each image frame one by one
+# loop to iterate each image frame one by one
 while True:
     success, image = capture.read()
-    # TODO: reducing the image size one fifth (.20) to speed up as the process is real time
+    #  reducing the image size one fifth (.20) to speed up as the process is real time
     small_image = cv2.resize(image, (0, 0), None, 0.20, 0.20)
-    # TODO: converting the smaller image from bgr to rgb
+    # converting the smaller image from bgr to rgb
     small_image = cv2.cvtColor(small_image, cv2.COLOR_BGR2RGB)
 
-    # TODO: enabling to locate multiple faces and sending all the face locations for face encoding in next step
+    # enabling to locate multiple faces and sending all the face locations for face encoding in next step
     facesInCurrentFrame = face_recognition.face_locations(small_image)
-    # TODO: image encoding
+    # image encoding
     currentFrameEncodings = face_recognition.face_encodings(small_image, facesInCurrentFrame) #CNN
     face_recognition.face_encodings(image)
 
-    # TODO: one by one it will grab one face location from faces current frame list...
+    # one by one it will grab one face location from faces current frame list...
     # ... and it will grab the encoding of encode face from encodes current face
     for encodeFace, faceLocation in zip(currentFrameEncodings, facesInCurrentFrame):
         matchings = face_recognition.compare_faces(encodingKnownList, encodeFace)
-        # TODO: calculating each face distance and the lowest face distance probability is the best match
+        # calculating each face distance and the lowest face distance probability is the best match
         faceDistance = face_recognition.face_distance(encodingKnownList, encodeFace)
 
-        # TODO: list of face distance
+        # list of face distance
         print(faceDistance)
 
         matchIndex = np.argmin(faceDistance)
 
-        # TODO: bounding box creation and writing the name
+        # bounding box creation and writing the name
         if matchings[matchIndex]:
             studentName = studentNames[matchIndex].upper()
             print(studentName)
 
             y1,x2,y2,x1 = faceLocation
-            # TODO: multiplying by 5 as the small image was one fifth of actual image
+            # multiplying by 5 as the small image was one fifth of actual image
             y1, x2, y2, x1 = y1*5, x2*5, y2*5, x1*5
             cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 255), 3)
             cv2.rectangle(image, (x1, y2 - 35), (x2, y2), (0, 255, 255), -1) #cv2.filled can be used instead of -1
-            # TODO: displaying name in the box
+            # displaying name in the box
             cv2.putText(image, studentName, (x1 + 6, y2 - 6), cv2.FONT_ITALIC, 1, (0, 0, 255), 3)
             markAttendance(studentName)
 
